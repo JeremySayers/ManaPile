bec736c323fede92f5f6ad184a162099013657f3
Sayers <jsayers@dcscorp.com>
2018-07-06 15:52:25 -0400

    Added the owned tag to the card images instead of just highlighting

--- edhrec.js
+++ edhrec.js
@@ -2,11 +2,40 @@ var globalCardList = ['test'];
 
 chrome.storage.sync.get(['cardList'], function(obj) {
     globalCardList = obj.cardList;
+    
+    var listLength = globalCardList.length;
+
+    for (var i = 0; i < listLength; i++){
+        card = globalCardList[i].split(" ");
+        if (!isNaN(card[0])) {
+            card.shift();
+            globalCardList[i] = card.join(" ");
+        }
+    }
+
     console.log(globalCardList);
     var walker = document.createTreeWalker(document.body, NodeFilter.SHOW_ELEMENT, myfilter, false);
     
     while (walker.nextNode()) {
-        walker.currentNode.style.border = '3px solid green';
+        var ownedDiv = document.createElement("div");
+        var content = document.createTextNode('Owned');
+        ownedDiv.appendChild(content);
+
+        ownedDiv.style.borderRadius = "5px";
+        ownedDiv.style.zIndex = "4";
+        ownedDiv.style.position = "absolute";
+        ownedDiv.style.left = ".5em";
+        ownedDiv.style.bottom = "1.5em";
+        ownedDiv.style.display = "block";
+        ownedDiv.style.color = "white";
+        ownedDiv.style.fontSize = "1em";
+        ownedDiv.style.backgroundColor = "#49ed4c";
+        ownedDiv.style.paddingLeft = "1px";
+        ownedDiv.style.paddingRight = "1px";
+        ownedDiv.className = "ownedDiv";
+        console.log("We Made a Div!");
+
+        walker.currentNode.childNodes[1].appendChild(ownedDiv);
     }
 })
 
